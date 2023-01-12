b881f94a360c55423617861a99160eb716bc345d
MaxIsACat <kanielrkirby@gmail.com>
2023-01-12 01:29:38 -0600

    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { UserContext, UserContextI } from './utils/User'
 
 function App() {
 	const { user, signInWithGoogle, signOut, editProfile } = useContext(UserContext) as UserContextI
 	return (
 		<div className='App'>
-			<h2>You are: </h2>
-			<p>Name: {user?.displayName}</p>
-			<p>ID: {user?.uid}</p>
-
-			<form action=''>
-				<button
-					onClick={async (e) => {
-						e.preventDefault()
-						await signInWithGoogle()
-					}}
-				>
-					Sign in with Google
-				</button>
-				<button
-					onClick={async (e) => {
-						e.preventDefault()
-						await signOut()
-					}}
-				>
-					Sign out
-				</button>
-			</form>
+			<Routes>
+				<Route path='/' element={<>home</>} />
+				<Route path='/notes' element={<>notes</>} />
+				<Route path='/notes/:id' element={<>note with id</>} />
+				<Route path='/login' element={<>login</>} />
+				<Route path='/signup' element={<>signup</>} />
+				<Route path='/settings' element={<>settings</>} />
+				<Route path='*' element={<>404</>} />
+			</Routes>
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import { UserProvider } from './utils/User'
 import './samples/node-api'
@@ -7,9 +8,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<UserProvider>
-			<App />
-		</UserProvider>
+		<Router>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</Router>
 	</React.StrictMode>,
 )
 
