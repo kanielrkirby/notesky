a2163ea2836af7a7e450f0617df1044234e01695
Max <kanielrkirby@gmail.com>
2023-03-18 14:40:35 -0500

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Note from "./pages/Note";
-// import Create from "./pages/Create";
-// import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Note from "./pages/Note";
+import Create from "./pages/Create";
+import Navbar from "./components/Navbar";
 
 function Wrapper() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Outlet />
     </>
   );
@@ -22,10 +24,13 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Wrapper />}>
         <Route index element={<Home />} />
-        {/* <Route path="about" element={<About />} /> */}
-        {/* <Route path="note/:id" element={<Note />} /> */}
-        {/* <Route path="create" element={<Create />} /> */}
+        <Route path="about" element={<About />} />
+        <Route path="note/:id" element={<Note />} />
+        <Route path="create" element={<Create />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
